96ccc9322479acefd25d2d84cec42ec0cc3b75e2
Mannava Gopi Krishna <144347344+Gopi9989@users.noreply.github.com>
2024-12-04 22:47:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,173 @@
+function textColourRed(){
+    let t=document.getElementById('text')
+    t.style.color="red"
+}
+function textColourGreen(){
+    let t=document.getElementById("text")
+    t.style.color="green"
+}
+function textColourBlue(){
+    let t=document.getElementById("text")
+    t.style.color="blue"
+}
+function textColourViolet(){
+    let t=document.getElementById("text")
+    t.style.color="violet"
+}
+function textColourIndigo(){
+    let t=document.getElementById("text")
+    t.style.color="indigo"
+}
+function textColourYellow(){
+    let t=document.getElementById("text")
+    t.style.color="yellow"
+}
+function textColourOrange(){
+    let t=document.getElementById("text")
+    t.style.color="orange"
+}
+function textColourSkyblue(){
+    let t=document.getElementById("text")
+    t.style.color="skyblue"
+}
+function textColourSalmon(){
+    let t=document.getElementById("text")
+    t.style.color="salmon"
+}
+function textColourYellowgreen(){
+    let t=document.getElementById("text")
+    t.style.color="yellowgreen"
+}
+function textColourRoyalblue(){
+    let t=document.getElementById("text")
+    t.style.color="royalblue"
+}
+function textColourTomato(){
+    let t=document.getElementById("text")
+    t.style.color="tomato"
+}
+function textColourTeal(){
+    let t=document.getElementById("text")
+    t.style.color="teal"
+}
+function textColourMediumturquoise(){
+    let t=document.getElementById("text")
+    t.style.color="mediumturquoise"
+}
+function textColourFirebrick(){
+    let t=document.getElementById("text")
+    t.style.color="firebrick"
+}
+
+
+
+function backColourRed(){
+    let t=document.getElementById('text')
+    t.style.backgroundColor="red"
+}
+function backColourGreen(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='green'
+}
+function backColourBlue(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='blue'
+}
+function backColourViolet(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='violet'
+}
+function backColourIndigo(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='indigo'
+}
+function backColourYellow(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='yellow'
+}
+function backColourOrange(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='orange'
+}
+function backColourSkyblue(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='skyblue'
+}
+function backColourSalmon(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='Salmon'
+}
+function backColourYellowgreen(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='yellowgreen'
+}
+function backColourRoyalblue(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='royalblue'
+}
+function backColourTomato(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='tomato'
+}
+function backColourTeal(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='teal'
+}
+function backColourMediumturquoise(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='mediumturquoise'
+}
+function backColourFirebrick(){
+    let t=document.getElementById("text")
+    t.style.backgroundColor='firebrick'
+}
+function fontGeorgia(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Georgia"
+}
+function fontTimes(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Times New Roman"
+}
+function fontBokor(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Bokor"
+}
+function fontCouriernew(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Courier New"
+}
+function fontVerdana(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Verdana"
+}
+function fontYujimai(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Yuji Mai"
+}
+function fontComicsans(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Comic Sans MS"
+}
+function fontTrebuchet(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Trebuchet MS"
+}
+function fontTahoma(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Tahoma"
+}
+function fontArial(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Arial"
+}
+function fontRoboto(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Roboto"
+}
+function fontJaro(){
+    let t=document.getElementById("text")
+    t.style.fontFamily="Jaro"
+}
+
+
